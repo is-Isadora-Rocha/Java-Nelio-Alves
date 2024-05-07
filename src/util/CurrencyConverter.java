48068f160e530a7cc77bd08119f921e9e347d235
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-05-07 11:20:16 -0300

    fix cotação do dolar

--- src/util/CurrencyConverter.java
+++ src/util/CurrencyConverter.java
@@ -6,11 +6,15 @@ public class CurrencyConverter {
 
     public static double calcIOF(double price){
         return price * IOF;
+        // valor do dolar * 0.06
     }
 
     public static double convertDolartoReal (double price, double dollars){
         double realValue = price * dollars;
+        // valor do dolar * valor em dolar
         double iofValue = calcIOF(realValue);
+        //iofValue recebe o valor calculado do método calcIOF
         return realValue + iofValue;
+        // valor do real + valor calculado com IOF
     }
 }
