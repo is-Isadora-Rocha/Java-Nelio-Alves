ecba3ba7e38565fdacfdde54c771b57681e48047
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-05-07 09:07:17 -0300

    metodos estaticos

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,42 +1,31 @@
 package application;
 
+import util.Calculator;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-    public static final double PI = 3.14159;
-    // final = valor constante, inalterável
-    // padrão de constante usa letra maiúscula
-
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        // obj instanciado obrigatoriamente por não ser estático
+        Calculator calc = new Calculator();
+
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = circumference(radius);
+        double c = calc.circumference(radius);
 
-        double v = volume(radius);
+        double v = calc.volume(radius);
 
         System.out.printf("Circunference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", PI);
+        System.out.printf("PI value: %.2f%n", calc.PI);
 
         sc.close();
-
-        // NÃO PODE CHAMAR MÉTODOS NÃO-ESTÁTICO NUM MÉTODO ESTÁTICO (main é um método estático)
-        // por exemplo: se os métodos circumference e volume não fossem static, não poderiam está na classe main
-    }
-
-    // SÃO METÓDOS ESTÁTICOS POIS ESTÃO SENDO DECLARADOS DENTRO DE UM MÉTODO ESTÁTICO (main)
-    public static double circumference (double radius){
-        return 2.0 * PI * radius;
-    }
-
-    public static double volume (double radius){
-        return 4.0 * PI * radius * radius * radius / 3.0;
     }
 
 }
--- src/util/Calculator.java
+++ src/util/Calculator.java
@@ -1,4 +1,16 @@
 package util;
 
 public class Calculator {
+    public final double PI = 3.14159;
+    // final = valor constante, inalterável
+    // padrão de constante usa letra maiúscula
+
+    // SÃO METÓDOS ESTÁTICOS POIS ESTÃO SENDO DECLARADOS DENTRO DE UM MÉTODO ESTÁTICO (main)
+    public double circumference (double radius){
+        return 2.0 * PI * radius;
+    }
+
+    public double volume (double radius){
+        return 4.0 * PI * radius * radius * radius / 3.0;
+    }
 }
