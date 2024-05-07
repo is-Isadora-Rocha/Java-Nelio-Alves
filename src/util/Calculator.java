812bbf67d6d51b72f852511887b77375d29b5d6c
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-05-07 09:19:34 -0300

    metodos estaticos final

--- src/util/Calculator.java
+++ src/util/Calculator.java
@@ -1,16 +1,14 @@
 package util;
 
 public class Calculator {
-    public final double PI = 3.14159;
-    // final = valor constante, inalterável
-    // padrão de constante usa letra maiúscula
+    public static final double PI = 3.14159;
+    //independete de objetos
 
-    // SÃO METÓDOS ESTÁTICOS POIS ESTÃO SENDO DECLARADOS DENTRO DE UM MÉTODO ESTÁTICO (main)
-    public double circumference (double radius){
+    public static double circumference (double radius){
         return 2.0 * PI * radius;
     }
 
-    public double volume (double radius){
+    public static double volume (double radius){
         return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
